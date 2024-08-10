9eb171f9c592fdc99845694e574b033f1eb071be
Paweł Dąbrowski <dziamber@gmail.com>
2024-08-10 19:29:52 +0200

    Remove localhost from fetcher

--- lib/rails_plan/cli/fetch_template.rb
+++ lib/rails_plan/cli/fetch_template.rb
@@ -3,8 +3,7 @@
 module RailsPlan
   module Cli
     class FetchTemplate
-      # API_URL = 'https://railsplan.com/api/v1'
-      API_URL = 'http://localhost:3000/api/v1'
+      API_URL = 'https://railsplan.com/api/v1'
 
       def call(uid)
         response = RestClient.get(API_URL + "/plans/#{uid}") { |res| res }
